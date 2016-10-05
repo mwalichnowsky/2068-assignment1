c0088e6e65110f89168a8499faf860fcdafe82ff
mwalichnowsky <mwalichnowsky@hotmail.com>
2016-10-04 23:22:32 -0400

    first commit

--- assignment1/2068-assignment1/routes/index.js
+++ assignment1/2068-assignment1/routes/index.js
@@ -6,23 +6,10 @@ router.get('/', function(req, res, next)
 {
   res.render('index', 
   {   
-    title: 'COMP2068 | Lesson 4 - Part 2', 
-    message:'This is the lesson 4 homepage' 
+    title: 'COMP2068 | Assignment 1', 
+    message:'This is the assignment 1 homepage' 
   });
 });
 
-/* Get random number page */
-router.get('/random', function(req, res, next)
-{
-  // Calculate a random number.
-  var num = Math.random();
-
-  // Load the random page and pass the num into it.
-  res.render('random', 
-  {
-    title: 'Random Number',
-    randomNumber: num
-  })
-}
 
 module.exports = router;
